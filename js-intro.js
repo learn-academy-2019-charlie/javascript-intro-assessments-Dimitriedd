49ad74e81d0dc1d28ecb0dab0c48afb4624e1993
Ubuntu <ubuntu@ip-172-31-2-44.us-east-2.compute.internal>
2019-06-07 22:46:39 +0000

    js-intro-assessment Finished

--- js-intro.js
+++ js-intro.js
@@ -3,20 +3,75 @@
 // Consider this variable:
 
 var mantra = "Be the dev";
-
+var test = "EvvvEBEExxxxEE"
 // 1. Write the code that returns 'B' from mantra.
+const findB=(word)=>{
+    let wordLower = word.toLowerCase("")
+    for(let i=0;i<wordLower.length;i++){
+        if(wordLower.substr(i,1)==="b"){
+            return word.substr(i,1)
+        }
+    }
+    
+}
+console.log(findB(test))
+console.log(findB(mantra))
 
 // 2. Write the code that determines if there is a 'x' in mantra.
-
+const findX=(word)=>{
+    let catchAll=true
+    let wordLower = word.toLowerCase("")
+    for(let i=0;i<wordLower.length;i++){
+        if(wordLower.substr(i,1)==="x"){
+            catchAll=false
+            return word.substr(i,1)
+        }
+    }if(catchAll===true){
+        return "There is no x"
+    }else{
+        return"error"
+    }
+    
+}
+console.log(findX(test))
+console.log(findX(mantra))
 // 3. Write the code that determines if there is a 'v' in mantra.
-
+const findV=(word)=>{
+    let catchAll=true
+    let wordLower = word.toLowerCase("")
+    for(let i=0;i<wordLower.length;i++){
+        if(wordLower.substr(i,1)==="v"){
+            catchAll=false
+            return word.substr(i,1)
+        }
+    }if(catchAll===true){
+        return "There is no x"
+    }else{
+        return"error"
+    }
+    
+}
+console.log(findV(test))
+console.log(findV(mantra))
 // Stretch: Write the code that returns the position of 'v' in mantra.
+const findVStretch=(word)=>{
+    let wordLower = word.toLowerCase("")
+    for(let i=0;i<wordLower.length;i++){
+        if(wordLower.substr(i,1)==="v"){
+            return `${word.substr(i,1)} is number ${i} in the string`
+        }
+    }
+    
+}
+
+console.log(findVStretch(test))
+console.log(findVStretch(mantra))
 
 
 // Consider the following statement:
 
-var codingIsEasy
-var imBored
+var codingIsEasy = true
+var imBored = true
 
 if (codingIsEasy && imBored) {
     console.log("I need more!")
@@ -28,8 +83,8 @@ if (codingIsEasy && imBored) {
 
 // Consider the following statement:
 
-var imLost
-var imFrustrated
+var imLost=false
+var imFrustrated=false
 
 if (imLost || imFrustrated) {
     console.log('Break time!')
@@ -40,8 +95,16 @@ if (imLost || imFrustrated) {
 // 5. Update the imLost and imFrustated variables above the statement to the values required to log "Keep coding!"
 
 // 6a. Recall the mantra variable from the first question. Write the code that logs each letter of mantra using a for loop.
+for(let i=0;i<mantra.length;i++){
+    console.log(mantra.substr(i,1))
+    }
 
 // Stretch: Using a while loop.
+var myWhileLoop= 0
+while(myWhileLoop < 4){
+    myWhileLoop++
+    console.log(myWhileLoop)
+}
 
 // Super Stretch: Using forEach().
 
@@ -50,6 +113,25 @@ if (imLost || imFrustrated) {
 var message = "thisisateststring"
 
 // 7. Write a function that takes a string like message as an argument and returns the string without vowels.
+
+var noVowels=(msg)=>{
+    let newMsg=""
+
+    if(typeof msg ==="string"){
+        let lowerMsg=msg.toLocaleLowerCase()
+        for(let i=0;i<lowerMsg.length;i++){
+            if(lowerMsg.substr(i,1) !== "a"&&lowerMsg.substr(i,1) !=="e"&&lowerMsg.substr(i,1) !=="i"&&lowerMsg.substr(i,1) !=="o"&&lowerMsg.substr(i,1) !=="u"){
+            newMsg=newMsg+(lowerMsg.substr(i,1))
+            }
+        }
+        return newMsg
+    }else{
+        return `Please enter a string and not a ${typeof msg}`
+    }
+}
+console.log(noVowels(message))
+console.log(noVowels(1234))
+
 
 // Stretch: Update your function to throw an exception if the argument is not a string.
 
@@ -60,3 +142,13 @@ var message = "thisisateststring"
 var toonimals = [ {name: 'Itchy', animal: 'mouse'}, {name: 'Stimpy', animal: 'cat'}, {name: 'Daffy', animal: 'duck'}, {name: 'Scratchy', animal: 'cat'}, {name: 'Ren', animal: 'dog'}, {name: 'Felix', animal: 'cat'}]
 
 // 8. Write a function that takes an array like toonimals and returns an array with only the toons that are cats.
+const catsOnly=(arr)=>{
+  let newArr=[]
+  for(let i=0;i<arr.length;i++){
+      if(arr[i].animal ==='cat'){
+          newArr.push(arr[i])
+      }
+  }
+  return newArr
+}
+console.log(catsOnly(toonimals))
--- js-intro.md
+++ js-intro.md
@@ -3,15 +3,24 @@
 Answer these questions, then follow the instructions to submit your first assessment!
 
 #### 1. What is a function? Why would you use one?
-
-#### random sentence
+A function is a part of code that can be called apon with an argument
 
 #### 2. What is a method?
+Its like a operator that you can put at the end of varibles to modify or acces something in it
 
 #### 3. What are some examples of javascript string methods?
+str.substr(a,b) is a method that allows you to acces certin parts of the string
+str.toLowerCase is a method that changes the case in the string to lower case
+str.split("") is a method that turns a string into an array
+str.length is a method that returns the strings length
 
 #### 4. Explain the difference between accessor and mutator methods. Why would you use one over the other?
+An accessor is a method that acceses a part of a string without modifying the base string
+A mutator is a method that changes the string based on the method
 
 #### 5. Explain the differences between console.log() and return. Why would you use one over the other?
+console.log logs something to the console and only to the console
+return returns data to something that called it
 
 #### 6. What is one thing we did in class this week that helped you learn the most?  
+I think that the lerctures really helped me understand the conceps but pair codding help me practice them too
